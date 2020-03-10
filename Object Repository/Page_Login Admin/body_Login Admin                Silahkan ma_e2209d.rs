7d9d870111af65bfc964a3b0e39adf4cd731eea0
MIS-04 <evakristina2603@gmail.com>
2020-03-10 18:39:24 +0700

    post testing lagi

---
+++ Object Repository/Page_Login Admin/body_Login Admin                Silahkan ma_e2209d.rs
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_Login Admin                Silahkan ma_e2209d</name>
+   <tag></tag>
+   <elementGuidId>3184e898-3cc3-4c95-8f44-5b4d8500259f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+    
+        
+            
+                Login Admin
+                Silahkan masuk ke Panel Admin
+            
+        
+        
+            
+                
+                    
+                        Email
+                        
+                    
+                    
+                        Password
+                        
+                    
+                    
+                        
+                            
+                                
+                                 Ingat Saya
+                            
+                            Lupa Password?
+                        
+                    
+                    
+                        
+                    
+
+                
+            
+        
+    
+
+
+
+/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/tes login.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>tes login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c5ac167a-5e30-49d6-b316-c232fd031116</testSuiteGuid>
+   <testCaseLink>
+      <guid>4d034f1a-5c20-4838-9bc8-f21f59df9214</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login - salah</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>17c1e080-4e3a-4216-87a5-3b42b46cd310</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
