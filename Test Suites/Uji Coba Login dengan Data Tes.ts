64863abde6669235693bb5ccc8bef610a90697f0
Eva Kristina <evakristina2603@gmail.com>
2020-05-02 23:21:45 +0700

    tes variabel

---
+++ Test Suites/Uji Coba Login dengan Data Tes.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Uji Coba Login dengan Data Tes</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a989395d-6ee9-4cba-9c34-8b7cd97a8ec2</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b9f1a75-20c3-4baf-a3fd-0ffa0d03591a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/login dian kopi - variable</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>5b6720a3-6f1c-452a-9658-0dd187777f3e</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Data Login</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>5b6720a3-6f1c-452a-9658-0dd187777f3e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>ba1ed2ab-d3a0-4a58-9a6c-c20bfa865430</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>5b6720a3-6f1c-452a-9658-0dd187777f3e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>87943b2a-cf60-42cf-9f74-76814a7ef5b3</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
